bcbda7bcd82c0dead82f28b3ed826f5f05e11902
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2024-07-18 17:37:27 +0530

    Netlify badge updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
+  integrations: [
 		starlight({
 			title: 'DiceDB',
       favicon: '/favicon.png',
