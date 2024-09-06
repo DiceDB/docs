17a4d1890006e4ac34d5e578fa442592a70cb215
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2024-09-06 18:53:10 +0530

    Landing with blog with docs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,15 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
+const blog = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		published_at: z.coerce.date(),
+	}),
+});
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+    blog,
+    docs: defineCollection({ schema: docsSchema() }),
 };
